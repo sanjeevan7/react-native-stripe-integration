5a90f8db7a739df4e40ac22b5d615dc41c9c79c1
sanjeevan1607 <57339152+sanjeevan1607@users.noreply.github.com>
2020-11-20 20:55:03 +0530

    react-native-stripe-integration

---
+++ App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { AppLoading, Asset, Font, Icon } from 'expo';
+import AppNavigator from './src/navigation/AppNavigator';
+
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false,
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+          <AppNavigator />
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ src/navigation/MainTabNavigator.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import AddSubscription from '../screens/AddSubscriptionScreen';
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={
+        Platform.OS === 'ios'
+          ? `ios-information-circle${focused ? '' : '-outline'}`
+          : 'md-information-circle'
+      }
+    />
+  ),
+};
+
+const AddSubscriptionStack = createStackNavigator({
+  Links: AddSubscription,
+});
+
+AddSubscriptionStack.navigationOptions = {
+  tabBarLabel: 'Subscribe',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? 'ios-cash' : 'md-cash'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  AddSubscription: { screen: AddSubscriptionStack, navigationOptions:{tabBarVisible: false} },
+  HomeStack
+});
